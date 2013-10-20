5654101a1de1763d8c0de3bc70f8377e275d2367
Adam Lavin <adam@lavoaster.co.uk>
2013-10-20 18:22:29 +0100

    Convert Route::resource to Route::group

--- app/BattleNexus/User/routes.php
+++ app/BattleNexus/User/routes.php
@@ -4,7 +4,15 @@
 Route::get('login', ['as' => 'user.login', 'uses' => 'BattleNexus\User\Controllers\UserController@login']);
 Route::post('login', ['as' => 'user.authorize', 'uses' => 'BattleNexus\User\Controllers\UserController@authorize']);
 
-Route::resource('user', 'BattleNexus\User\Controllers\UserController');
+Route::group(['prefix' => 'user'], function()
+{
+	Route::get('/', ['as' => 'user.index', 'uses' => 'BattleNexus\User\Controllers\UserController@index']);
+	Route::post('/', ['as' => 'user.store', 'uses' => 'BattleNexus\User\Controllers\UserController@store']);
+	Route::get('{id}', ['as' => 'user.show', 'uses' => 'BattleNexus\User\Controllers\UserController@show']);
+	Route::get('{id}/edit', ['as' => 'user.edit', 'uses' => 'BattleNexus\User\Controllers\UserController@edit']);
+	Route::patch('{id}', ['as' => 'user.update', 'uses' => 'BattleNexus\User\Controllers\UserController@update']);
+	Route::delete('{id}', ['as' => 'user.delete', 'uses' => 'BattleNexus\User\Controllers\UserController@destroy']);
+});
 
 Route::get('register', ['as' => 'user.create', 'uses' => 'BattleNexus\User\Controllers\UserController@create']);
 Route::post('register', ['as' => 'user.store', 'uses' => 'BattleNexus\User\Controllers\UserController@store']);
